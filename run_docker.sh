56c3af5939f66657fa1bca3031f8ea1b1d88f719
ashirborah <ashiraseesh@gmail.com>
2023-05-11 10:28:17 -0700

    Changed paths in docker. More to change

--- run_docker.sh
+++ run_docker.sh
@@ -3,8 +3,21 @@
 # Build the Docker image
 docker build -t switch_finder_image .
 
+## Mount the current directory to the Docker container
 # Run the pipeline
-docker run switch_finder_image python /khayyam/khorms/programs/SwitchFinder/SwitchFinder/wrappers/SwitchFinder_pipeline.py --input_fastafile /khayyam/khorms/programs/SwitchFinder/example_data/example_sequences.fa --out /khayyam/khorms/temp/SwitchFinder/out --temp_folder /khayyam/khorms/temp/SwitchFinder/temp --RNAstructure_path /khayyam/khorms/programs/RNAstructure --RNApathfinder_path /khayyam/khorms/programs/RNApathfinder
+docker -v {pwd}:/SwitchFinder run switch_finder_image python \
+        /SwitchFinder/SwitchFinder/wrappers/SwitchFinder_pipeline.py \
+        --input_fastafile /SwitchFinder/example_data/example_sequences.fa \
+        --out /khayyam/khorms/temp/SwitchFinder/out \
+        --temp_folder /khayyam/khorms/temp/SwitchFinder/temp \
+        --RNAstructure_path $RNAstructure_path \
+        --RNApathfinder_path $RNApathfinder_path
 
 # Run the new classifier script
-docker run switch_finder_image python /khayyam/khorms/programs/SwitchFinder/SwitchFinder/wrappers/new_classifier.py --input_fastafile /khayyam/khorms/temp/SwitchFinder/seed_riboswitches.fa --temp_folder /khayyam/khorms/temp/SwitchFinder/temp --RNAstructure_path /khayyam/khorms/programs/RNAstructure --RNApathfinder_path /khayyam/khorms/programs/RNApathfinder --fragment_length 200
+docker -v {pwd}:/SwitchFinder run switch_finder_image \
+python /SwitchFinder/SwitchFinder/wrappers/new_classifier.py \
+--input_fastafile /khayyam/khorms/temp/SwitchFinder/seed_riboswitches.fa \
+--temp_folder /khayyam/khorms/temp/SwitchFinder/temp \
+--RNAstructure_path $RNAstructure_path \
+--RNApathfinder_path $RNApathfinder_path \
+--fragment_length 200
